7ba1ee389721e95d4418e276773494dd91133dfa
Emi <empatnewman@gmail.com>
2015-01-25 05:04:48 -0800

    GOOGLE LOGIN

--- app/src/main/java/com/example/emilynewman/partytech/MainActivity3.java
+++ app/src/main/java/com/example/emilynewman/partytech/MainActivity3.java
@@ -1,5 +1,6 @@
 package com.example.emilynewman.partytech;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.support.v7.app.ActionBarActivity;
@@ -9,10 +10,14 @@ import android.view.MenuItem;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
 import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.*;
 
 
-public class MainActivity3 extends ActionBarActivity {
+public class MainActivity3 extends ActionBarActivity
+        implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+
 
     /* Request code used to invoke sign in user interactions. */
     private static final int RC_SIGN_IN = 0;
@@ -26,25 +31,75 @@ public class MainActivity3 extends ActionBarActivity {
     private boolean mIntentInProgress;
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_activity3);
 
+        // Create a GoogleApiClient instance
         mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
                 .build();
+
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // Connected to Google Play services!
+        // The good stuff goes here.
+    }
+
+    public void onDisconnected() {
+
+    }
+
+    public void onConnectionSuspended(int cause) {
+        mGoogleApiClient.connect();
+    }
+
+    public void onConnectionFailed(ConnectionResult result) {
+        if (!mIntentInProgress && result.hasResolution()) {
+            try {
+                mIntentInProgress = true;
+                startIntentSenderForResult(result.getResolution().getIntentSender(),
+                        RC_SIGN_IN, null, 0, 0, 0);
+            } catch (IntentSender.SendIntentException e) {
+                // The intent was canceled before it was sent.  Return to the default
+                // state and attempt to connect to get an updated ConnectionResult.
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }
     }
     protected void onStart() {
         super.onStart();
         mGoogleApiClient.connect();
     }
+
     protected void onStop() {
         super.onStop();
-        if (mGoogleApiClient.isConnected())
+
+        if (mGoogleApiClient.isConnected()) {
             mGoogleApiClient.disconnect();
+        }
     }
+
+    protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        if (requestCode == RC_SIGN_IN) {
+            mIntentInProgress = false;
+
+            if (!mGoogleApiClient.isConnecting()) {
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+
+
+
+    /*-====================NON GOOGLE=============================*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -68,39 +123,5 @@ public class MainActivity3 extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /*
-     * Google+ login methods
-     *
-     */
-    public void onConnectionFailed(ConnectionResult result) {
-        if (!mIntentInProgress && result.hasResolution()) {
-            try {
-                mIntentInProgress = true;
-                startIntentSenderForResult(result.getResolution().getIntentSender(),
-                        RC_SIGN_IN, null, 0, 0, 0);
-            } catch (IntentSender.SendIntentException e) {
-                // The intent was canceled before it was sent.  Return to the default
-                // state and attempt to connect to get an updated ConnectionResult.
-                mIntentInProgress = false;
-                mGoogleApiClient.connect();
-            }
-        }
-    }
 
-    public void onConnected(Bundle connectionHint) {
-        // We've resolved any connection errors.  mGoogleApiClient can be used to
-        // access Google APIs on behalf of the user.
-    }
-    protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
-        if (requestCode == RC_SIGN_IN) {
-            mIntentInProgress = false;
-
-            if (!mGoogleApiClient.isConnecting()) {
-                mGoogleApiClient.connect();
-            }
-        }
-    }
-    public void onConnectionSuspended(int cause) {
-        mGoogleApiClient.connect();
-    }
 }
